4ee43242dea29abc1d022d529fbdbd50399f3b98
stevekahanya <wakahusteve@gmail.com>
2026-02-12 19:41:36 +0300

    updated vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/App.test.js', // Or a separate setup file if preferred
+  },
 })
